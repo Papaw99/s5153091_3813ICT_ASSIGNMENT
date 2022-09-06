d39951c54b88c86cf757b5d1669dedce616a727d
USAMA SIDDIQUI <usama.siddiqui5@gmail.com>
2022-09-07 00:31:16 +1000

    Added JSON data. Used fs to get JSON data and parser to convert JSON data.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const fs = require('fs')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 
@@ -10,6 +11,19 @@ const path = require('path')
 const http = require('http').Server(app)
 
 app.use(express.static(path.join(__dirname, '../dist/s5153091-3813-ict-assignment')))
+
+let usersRawData = fs.readFileSync("./data/users.json")
+let groupsRawData = fs.readFileSync("./data/groups.json")
+let channelsRawData = fs.readFileSync("./data/channels.json")
+let groupMembershipsRawData = fs.readFileSync("./data/groupMemberships.json")
+let channelMembershipsRawData = fs.readFileSync("./data/channelMembership.json")
+
+let users = JSON.parse(usersRawData)
+let groups = JSON.parse(groupsRawData)
+let channels = JSON.parse(channelsRawData)
+let groupMemberships = JSON.parse(groupMembershipsRawData)
+let channelMemberships = JSON.parse(channelMembershipsRawData)
+
 
 app.listen(3000, '127.0.0.1', function(){
     console.log('server has been started')
